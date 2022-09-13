ba9bd2f771dd343041a592941f6b5f678158eed9
Cassandrus <cassandrus@protonmail.com>
2022-09-13 18:06:44 +0300

    Work on contracts 21

--- std/MathLib.sol
+++ std/MathLib.sol
@@ -27,4 +27,38 @@ abstract contract MathLib {
 		);
     }	
 	
+	function $swap(int128[] array_, uint128 indexA_, uint128 indexB_) internal inline pure {
+		(array_[indexA_], array_[indexB_]) = (array_[indexB_], array_[indexA_]);
+	}
+	
+	function $avg(uint128 x, uint128 y) internal inline pure returns (uint128 result) {
+		return 	(x >> 1) + 
+				(y >> 1) + 
+				(x & y & 1);
+	}		
+	
+	function _sort(int256[] array, uint256 begin, uint256 end) internal pure {
+		if (begin < end) {
+			uint256 j = begin;
+			int256 pivot = array[j];
+			for (uint256 i = begin + 1; i < end; ++i) {
+				if (array[i] < pivot) {
+					$swap(array, i, ++j);
+				}
+			}
+			$swap(array, begin, j);
+			_sort(array, begin, j);
+			_sort(array, j + 1, end);
+		}
+	}	
+	
+	function _median(uint128[] sortedArray_, uint length_) internal pure returns(uint128) {
+		//sort(array, 0, length);
+		return length_ % 2 == 0 
+				? 
+				$avg(sortedArray_[length_/2-1], sortedArray_[length_/2]) 
+				: 
+				sortedArray_[length_/2];
+	}
+	
 }
